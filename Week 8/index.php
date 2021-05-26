b2aa0aac33c354302667319a1953b3d4baae088b
Harsh5chal <75268993+Harsh5chal@users.noreply.github.com>
2021-05-26 15:26:11 +0530

    Add files via upload

---
+++ Week 8/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title> Harsh panchal</title>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to  Rock Paper Scissors</h1>
+<p>
+<?php require_once "bootstrap.php"; ?>
+</p>
+<a href="login.php">Please Log In</a>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
+</body>
+</html>
